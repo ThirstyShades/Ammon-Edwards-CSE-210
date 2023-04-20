81ab100953679e567b3fe893dd5d1020e73f7d2f
Ammon Edwards <ammonscottedwards@gmail.com>
2023-04-20 09:09:01 -0600

    Added my name to the read me file and modified prep one to compleate the assiment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string F_name = Console.ReadLine(); 
+        Console.Write("What is your last name? ");
+        string L_name = Console.ReadLine();
+        Console.Write($"\nYour name is {L_name}, {F_name} {L_name}.");
     }
 }
