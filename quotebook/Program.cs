0c0c5dca03f8ac25f280ca3e240fa2b2c1ed8ae3
Ammon Edwards <ammonscottedwards@gmail.com>
2023-05-05 22:51:36 -0600

    Done with code along working as intended.

---
+++ quotebook/Program.cs
@@ -0,0 +1,18 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Source tateSource = new Source("Breathe Air - Andrew Tate | Tate On Vapes", "https://www.youtube.com/watch?v=uMZZ8WgM82k");
+
+// Console.WriteLine(tateSource.Stringify());
+
+Quote andrewTate = new Quote("Andrew Tate", "Breath air! you don't need a vape.", tateSource);
+
+// Console.WriteLine(andrewTate.GetQuote()); 
+
+Board quoteboard = new Board();
+
+quoteboard.AddQuote(andrewTate);
+quoteboard.FindQuotesByAuthor("Andrew Tate");
+
+Menu _menu = new Menu(quoteboard);
+_menu.Display();
