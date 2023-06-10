4e6b9998db4e3b46395cb191ffe1f7bf3fb89653
Ammon Edwards <ammonscottedwards@gmail.com>
2023-06-10 13:43:12 -0600

    DONE!

--- MindfulnessProgram/ListingActivity.cs
+++ MindfulnessProgram/ListingActivity.cs
@@ -2,9 +2,15 @@ public class ListingActivity : Activity
 {
     private string wellcomeMessage = $"Wellcome to the Listing Activity\nThis activity will help you reflect on the good things in your life by having you list as many things as you can in the given time frame";
 
+    List<string> _prompsList = new List<string>();
+
 
     public ListingActivity(int activityTime):base(activityTime)
     {
+    _prompsList.Add("What makes you happy?");
+    _prompsList.Add("What is your primary goals in life?");
+    _prompsList.Add("Ten years from now, where do you see yourself doing?");
+    _prompsList.Add("If you chould change anything you wan'ted about your life, what would you change?");
         
     }
 
@@ -18,7 +24,7 @@ public class ListingActivity : Activity
         Console.Clear();
         Console.WriteLine("List as many responces you can to the fallowing prompt:");
 
-        Console.WriteLine("Prompt Placeholder");
+        Console.WriteLine(GetRandomPrompt());
 
         Timer(); //5s timer
     }
@@ -46,6 +52,15 @@ public class ListingActivity : Activity
     {
         Console.WriteLine($"You have compleated another {_activityTime}s Listing Activity");
         Spinner();
+    }
+
+    public string GetRandomPrompt() //get random prompt to display to the user.
+    {
+        var random = new Random();
+        var randomNumber = random.Next(0,_prompsList.Count);
+        string randPrompt = _prompsList[randomNumber];
+        
+        return randPrompt;
     }
 
 
--- MindfulnessProgram/ReflectingActivity.cs
+++ MindfulnessProgram/ReflectingActivity.cs
@@ -2,8 +2,23 @@ public class ReflectingActivity : Activity
 {
     private string wellcomeMessage = $"Wellcome to the Reflecting Activity\nThis activity will help you reflect on times in yur life when you have shown strength and resilience.\nThis will help you recognize the power you have and how you can use it in other aspects of your life.";
 
+    List<string> _prompsList = new List<string>();
+    List<string> _reflectoinsList = new List<string>();
+
     public ReflectingActivity(int activityTime):base(activityTime)
     {
+        // prompts to reflect upon
+        _prompsList.Add("Think of a time when you stood up for someone else.?");
+        _prompsList.Add("Think of a time when you did something really difficult.");
+        _prompsList.Add("Think of a time when you helped someone in need.");
+        _prompsList.Add("Think of a time when you felt like you couldn't acompish your goals.");
+        _prompsList.Add("Think of a time when you did something truly selfless.");
+        //Reflectoin prompts
+        _reflectoinsList.Add("Why was this experience meaningful to you?");
+        _reflectoinsList.Add("Have you ever done anything like this before?");
+        _reflectoinsList.Add("How did you get started?");
+        _reflectoinsList.Add("How did you feel when it was complete?");
+        _reflectoinsList.Add("What made this time different than other times when you were not as successful?");
         
     }
 
@@ -16,7 +31,7 @@ public class ReflectingActivity : Activity
     {
         Console.WriteLine("Consider the fallowing prompt\n");
 
-        Console.WriteLine("prompt placeholder");//prompt fuction from list
+        Console.WriteLine(GetRandomPrompt());//prompt fuction from list
 
         Console.WriteLine("\nWhen you have somthing in mind, press enter to continue.");
         Console.ReadLine();
@@ -35,7 +50,7 @@ public class ReflectingActivity : Activity
         while(reflectingTime > 0)
         {
             
-            Console.WriteLine("Reflection Prompt Placeholder");//Display Refelct prompt
+            Console.WriteLine(GetRandomReflectoin());//Display Refelct prompt
             SpinnerTime5s(); //display spinner for 5s loop for deration
             reflectingTime = reflectingTime - 5;
             
@@ -49,5 +64,26 @@ public class ReflectingActivity : Activity
         Console.Clear();
         Console.WriteLine($"Well done!\nYou have sucesfully compleated another {_activityTime}s reflecting citivty!");
         Spinner();
+    }
+
+
+
+     public string GetRandomPrompt() //get random prompt to display to the user.
+    {
+        var random = new Random();
+        var randomNumber = random.Next(0,_prompsList.Count);
+        string randPrompt = _prompsList[randomNumber];
+        
+        return randPrompt;
+    }
+
+
+     public string GetRandomReflectoin() //get random prompt to display to the user.
+    {
+        var random = new Random();
+        var randomNumber = random.Next(0,_reflectoinsList.Count);
+        string randPrompt = _reflectoinsList[randomNumber];
+        
+        return randPrompt;
     }
 }
